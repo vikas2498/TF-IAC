6a1ff8e2031773965334fc3a29377bc604a445dd
vikas2498 <sharmavikas2498@gmail.com>
2026-03-01 11:49:20 +0530

    fixed nsg imports

--- import.tf
+++ import.tf
@@ -15,15 +15,15 @@ import {
 
 import {
     to = azurerm_subnet_network_security_group_association.app-nsg
-    id = "/subscriptions/33a960b5-98c2-4688-8fd8-3d5e673f9362/resourceGroups/sub-200-weu-dev-rg-net/providers/Microsoft.Network/networkSecurityGroups/sub-200-weu-dev-app-nsg"
+    id = "/subscriptions/33a960b5-98c2-4688-8fd8-3d5e673f9362/resourceGroups/sub-200-weu-dev-rg-net/providers/Microsoft.Network/virtualNetworks/sub-200-weu-dev-vnet/subnets/sub-200-weu-dev-app-snt"
 }
 
 import {
     to = azurerm_subnet_network_security_group_association.db-nsg
-    id = "/subscriptions/33a960b5-98c2-4688-8fd8-3d5e673f9362/resourceGroups/sub-200-weu-dev-rg-net/providers/Microsoft.Network/networkSecurityGroups/sub-200-weu-dev-db-nsg"
+    id = "/subscriptions/33a960b5-98c2-4688-8fd8-3d5e673f9362/resourceGroups/sub-200-weu-dev-rg-net/providers/Microsoft.Network/virtualNetworks/sub-200-weu-dev-vnet/subnets/sub-200-weu-dev-db-snt"
 }
 
 import {
     to = azurerm_subnet_network_security_group_association.pe-nsg
-    id = "/subscriptions/33a960b5-98c2-4688-8fd8-3d5e673f9362/resourceGroups/sub-200-weu-dev-rg-net/providers/Microsoft.Network/networkSecurityGroups/sub-200-weu-dev-pe-nsg"
+    id = "/subscriptions/33a960b5-98c2-4688-8fd8-3d5e673f9362/resourceGroups/sub-200-weu-dev-rg-net/providers/Microsoft.Network/virtualNetworks/sub-200-weu-dev-vnet/subnets/sub-200-weu-dev-pe-snt"
 }
