46bbad48a6746d153c2b3d01f8f1362b07b44625
vikas2498 <sharmavikas2498@gmail.com>
2026-03-01 11:40:42 +0530

    imported nsgs

--- import.tf
+++ import.tf
@@ -12,3 +12,18 @@ import {
     to = azurerm_subnet.pe
     id = "/subscriptions/33a960b5-98c2-4688-8fd8-3d5e673f9362/resourceGroups/sub-200-weu-dev-rg-net/providers/Microsoft.Network/virtualNetworks/sub-200-weu-dev-vnet/subnets/sub-200-weu-dev-pe-snt"
 }
+
+import {
+    to = azurerm_subnet_network_security_group_association.app-nsg
+    id = "/subscriptions/33a960b5-98c2-4688-8fd8-3d5e673f9362/resourceGroups/sub-200-weu-dev-rg-net/providers/Microsoft.Network/networkSecurityGroups/sub-200-weu-dev-app-nsg"
+}
+
+import {
+    to = azurerm_subnet_network_security_group_association.db-nsg
+    id = "/subscriptions/33a960b5-98c2-4688-8fd8-3d5e673f9362/resourceGroups/sub-200-weu-dev-rg-net/providers/Microsoft.Network/networkSecurityGroups/sub-200-weu-dev-db-nsg"
+}
+
+import {
+    to = azurerm_subnet_network_security_group_association.pe-nsg
+    id = "/subscriptions/33a960b5-98c2-4688-8fd8-3d5e673f9362/resourceGroups/sub-200-weu-dev-rg-net/providers/Microsoft.Network/networkSecurityGroups/sub-200-weu-dev-pe-nsg"
+}
